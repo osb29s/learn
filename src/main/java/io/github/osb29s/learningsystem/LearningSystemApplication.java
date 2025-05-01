1dd1831061aed286dd7650fb97148d7c6d1e08e9
rainett <96131229+rainett@users.noreply.github.com>
2025-05-01 17:11:16 +0300

    project setup

---
+++ src/main/java/io/github/osb29s/learningsystem/LearningSystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.osb29s.learningsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/osb29s/learningsystem/LearningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.osb29s.learningsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
